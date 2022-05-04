48f7e5751a863acb40685099146e8a01407b6656
torsumkhan <torsum.khan@gmail.com>
2022-05-05 00:38:47 +0500

    security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+
+  # http_basic_authenticate_with name: ENV["ADMIN_USERNAME_KEY"], password: ENV["ADMIN_PASSWORD_KEY"]
   def show
   end
 end
